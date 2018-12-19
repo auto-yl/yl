97c2187dd541e6b54d591a942c3901592910e3ff
only <776268047@qq.com>
2018-12-19 16:50:12 +0800

    YL

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ $(function(){
     function renderIndex(){
          var navHTML = "";
          $.each(this.globalParams.nav,function(n,m){
-             navHTML += "<li class='ui-li-static ui-body-inherit'><a href='" + m.url + "' data-transition='fade' class='li-a-style'>" + m.name + '</a></li>'
+             navHTML += "<li class='ui-li-static ui-body-inherit'><a href='" + m.url + "' data-transition='fade' class='li-a-style' target='_top'>" + m.name + '</a></li>'
          })
         $("#nav").html(navHTML)
     }
